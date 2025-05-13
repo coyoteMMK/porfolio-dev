a5f6119bca540651716d355107e10b9559dcecef
coyoteMMK <santosbaden@gmail.com>
2025-05-13 22:50:39 +0200

    index corregido y astro.config añadido site de github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://porfolio-dev.github.io',
+  base: 'my-porfolio',
   vite: {
     plugins: [tailwindcss()]
   }
