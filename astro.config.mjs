81d36f3b90575b337da552126b71871584e87fd6
coyoteMMK <santosbaden@gmail.com>
2025-05-14 00:13:31 +0200

    astro.config actualizado

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,8 +5,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://porfolio-dev.github.io',
-  base: 'my-porfolio',
+  site: 'https://coyoteMMK.github.io',
+  base: 'porfolio-dev.github.io',
   vite: {
     plugins: [tailwindcss()]
   }
