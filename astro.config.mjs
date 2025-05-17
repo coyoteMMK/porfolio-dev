7eda78a5c0616f1eb349813f42abb72be3e5af47
coyoteMMK <santosbaden@gmail.com>
2025-05-17 18:01:04 +0200

    delete: Remove site and base configuration from Astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,8 +5,6 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://coyoteMMK.github.io',
-  base: 'porfolio-dev.github.io',
   vite: {
     plugins: [tailwindcss()]
   }
